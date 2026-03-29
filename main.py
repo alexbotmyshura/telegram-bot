df13ff9bb2ef8604ee54cb3e6f29255dc3aa6d3c
alexbotmyshura <mishuraleksand@ukr.net>
2026-03-29 13:36:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
 import os
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🔥 Бот работает!")
+    keyboard = [
+        [InlineKeyboardButton("💰 BUY", callback_data="buy")],
+        [InlineKeyboardButton("📉 SELL", callback_data="sell")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Выбери действие:", reply_markup=reply_markup)
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+
+    if query.data == "buy":
+        await query.edit_message_text("💰 Покупка выбрана")
+    elif query.data == "sell":
+        await query.edit_message_text("📉 Продажа выбрана")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
+app.add_handler(CallbackQueryHandler(button))
 
 app.run_polling()
