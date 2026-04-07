1017b8dbe0510727e8be8f955412d05299536280
alexbotmyshura <mishuraleksand@ukr.net>
2026-04-07 04:45:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
-import os
-import time
-import hmac
-import hashlib
-import requests
-import json
-
-API_KEY = os.getenv("BYBIT_API_KEY")
-API_SECRET = os.getenv("BYBIT_API_SECRET")
-TOKEN = os.getenv("BOT_TOKEN")
-
-
 def place_order(side):
     url = "https://api.bybit.com/v5/order/create"
+
     recv_window = "5000"
     timestamp = str(int(time.time() * 1000))
 
@@ -22,12 +9,14 @@ def place_order(side):
         "symbol": "BTCUSDT",
         "side": side,
         "orderType": "Market",
-        "quoteOrderQty": "10"
+        "qty": "10",
+        "marketUnit": "quoteCoin"
     }
 
     body_str = json.dumps(body, separators=(",", ":"))
 
     param_str = timestamp + API_KEY + recv_window + body_str
+
     signature = hmac.new(
         bytes(API_SECRET, "utf-8"),
         param_str.encode("utf-8"),
@@ -36,39 +25,15 @@ def place_order(side):
 
     headers = {
         "X-BAPI-API-KEY": API_KEY,
+        "X-BAPI-SIGN": signature,
         "X-BAPI-TIMESTAMP": timestamp,
         "X-BAPI-RECV-WINDOW": recv_window,
-        "X-BAPI-SIGN": signature,
         "Content-Type": "application/json"
     }
 
-    response = requests.post(url, headers=headers, data=body_str)
-    print(response.text)
+    response = requests.post(url, headers=headers, data=body_str, timeout=20)
 
+    print("STATUS:", response.status_code)
+    print("RESPONSE:", response.text)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [
-        [InlineKeyboardButton("💰 BUY", callback_data="buy")],
-        [InlineKeyboardButton("📉 SELL", callback_data="sell")]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text("Выбери действие:", reply_markup=reply_markup)
-
-
-async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
-
-    if query.data == "buy":
-        place_order("Buy")
-        await query.edit_message_text("💰 Покупка отправлена")
-    elif query.data == "sell":
-        place_order("Sell")
-        await query.edit_message_text("📉 Продажа отправлена")
-
-
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CallbackQueryHandler(button))
-
-app.run_polling()
+    return response.text
